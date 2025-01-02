0ad5971c2d057e1fa91e3dcc5add82f4e4b3abc0
Mansichawla <medcod9@gmail.com>
2025-01-02 18:04:46 +0530

    changes in admindashboard

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -2,35 +2,55 @@ import React from "react";
 
 const AdminDashboard = () => {
   return (
-    <div className="h-screen w-fullp-10">
-      <Header />
-      <div>
-        <form className="flex flex-wrap w-full bg-red-200 items-start justify-between">
+    <div className="h-sreen w-full p-7 ">
+      <div className=" p-5 bg-[#1c1c1c] mt-7 rounded">
+        <form className="flex flex-wrap w-full bg-black-200 items-start justify-between">
           <div className="w-1/2">
             <div>
-              <h3>Task Title</h3>
-              <input type="text" placeholder="Make a UI Design"></input>
+              <h3 className="text-sm text-gray-300 mb-0.5">Task Title</h3>
+              <input
+                className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-grey-400 mb-4"
+                type="text"
+                placeholder="Make a UI Design"
+              ></input>
             </div>
             <div>
-              <h3>Date</h3>
-              <input type="date"></input>
+              <h3 className="text-sm text-gray-300 mb-0.5">Date</h3>
+              <input
+                className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-grey-400 mb-4"
+                type="date"
+              ></input>
             </div>
             <div>
-              <h3>Assign to</h3>
-              <input type="text" placeholder="employee name"></input>
+              <h3 className="text-sm text-gray-300 mb-0.5">Assign to</h3>
+              <input
+                className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-grey-400 mb-4"
+                type="text"
+                placeholder="employee name"
+              ></input>
             </div>
             <div>
-              <h3>Category</h3>
-              <input type="text" placeholder="design,dev, etc"></input>
-            </div>
-            <div>
-              <button>Create Task</button>
+              <h3 className="text-sm text-gray-300 mb-0.5">Category</h3>
+              <input
+                className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-grey-400 mb-4"
+                type="text"
+                placeholder="design,dev, etc"
+              ></input>
             </div>
           </div>
 
-          <div className="w-1/2">
-            <h3>Description</h3>
-            <textarea name="" id="" cols="30" rows=""></textarea>
+          <div className="w-2/5 flex flex-col items-start">
+            <h3 className="text-sm text-gray-300 mb-0.5">Description</h3>
+            <textarea
+              className="w-full h-44 text-sm py-2 px-4 rounded outline-none bg-transparent border-[1px] border-gray-400"
+              name=""
+              id=""
+              cols="30"
+              rows=""
+            ></textarea>
+            <button className="bg-emerald-500 py-3 hover:bg-emerald-600 px-5 rounded text-sm mt-4 w-full">
+              Create Task
+            </button>
           </div>
         </form>
       </div>
