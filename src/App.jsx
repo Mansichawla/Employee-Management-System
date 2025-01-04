62bd87c2a0530a0caf64871d1ed70123c323045e
Mansichawla <medcod9@gmail.com>
2025-01-04 22:21:25 +0530

    localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import "./App.css";
 const App = () => {
   return (
     <>
-      {/* <Login /> */}
-      <EmployeeDashboard />
+      <Login />
+      {/* <EmployeeDashboard /> */}
       {/* <AdminDashboard /> */}
     </>
   );
--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import CreateTask from "../Others/createTask";
+import AllTask from "../Others/AllTask";
 
 const AdminDashboard = () => {
   return (
     <div className="h-sreen w-full p-7 ">
       <h1 className="text-3xl font-medium">Good Morning, Mansi!</h1>
       <CreateTask />
+      <AllTask />
     </div>
   );
 };
