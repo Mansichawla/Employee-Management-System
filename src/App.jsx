5bd535261763afdc4c849d83ed5575fd9fe34a71
Mansichawla <medcod9@gmail.com>
2025-01-05 20:52:48 +0530

    separating userData and admin data

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import Login from "./components/Auth/Login";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 import AdminDashboard from "./components/Dashboard/AdminDashboard";
 import "./App.css";
+import { AuthContext } from "./context/AuthProvider";
+
 import { getLocalStorage, setLocalStorage } from "./utils/localStorage";
 const App = () => {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(null);
+  const authData = useContext(AuthContext);
+  console.log(authData);
 
   const handleLogin = (email, password) => {
     if (email == "admin@me.com" && password == "123") {
       setUser("admin");
-      // console.log("admin");
     } else if (email == "user@me.com" && password == "123") {
       setUser("user");
-      // console.log("user");
     } else {
       alert("invalid credentials");
     }
